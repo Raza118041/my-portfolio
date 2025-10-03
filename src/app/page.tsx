4f4ced9d9618e5ac6fbcae37be2857dc02d2cc6c
Ali Raza <129832771+Raza118041@users.noreply.github.com>
2025-10-03 15:07:46 +0500

    add animation on hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,19 @@
-import React from 'react'
+"use client"
+import React, { useEffect } from 'react'
 import Hero from './components/Hero/Hero'
+import AOS from 'aos'
+import 'aos/dist/aos.css'
+
 
 const Page = () => {
+  useEffect(() => {
+    AOS.init({
+      duration: 1000,
+      easing: 'ease-in-out',
+      once: false,
+      offset: 100
+    })
+  }, [])
   return (
     <div>
       <Hero />
